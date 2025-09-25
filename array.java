aeeae3eb88b84d2ce1edce2c4178a9d3b14c1e0a
clairez09 <clairezhao0919@gmail.com>
2025-09-25 08:59:02 -0700

    typed some stuff

--- array.java
+++ array.java
@@ -1,11 +1,16 @@
+import java.util.Scanner;
 public class array {
     public static void main(String[] args) {
-        int[] numbers = {1,2,3,4,5};
+        int[] numbers = {0,0,0,0,0};
         int sum = 0;
+        Scanner scanner = new Scanner(System.in);
         for (int i=0;i<5;i++){
+            System.out.println("Print an integer: ");
+            numbers[i] = scanner.nextInt();
             sum+=numbers[i];
         }
     System.out.println(sum);
+    scanner.close();
     }
 }
 
